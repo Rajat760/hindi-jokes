d0cdb6c0d07c821a05a91515db4b281ebcb1acdc
Rajat Mishra <88202566+Rajat760@users.noreply.github.com>
2023-03-24 22:30:28 +0530

    Update script.js

--- script.js
+++ script.js
@@ -4,12 +4,12 @@ copyBtn = document.querySelector(".copy"),
 twitterBtn = document.querySelector(".twitter")
 function randomQuote(){
     quoteBtn.classList.add("loading");
-    quoteBtn.innerText = "Loading Quote...";
+    quoteBtn.innerText = "Loading Joke...";
     fetch("https://hindi-jokes-api.onrender.com/jokes?api_key=94fd1f1570176f6c1b0501c6e380").then(response => response.json()).then(result => {
         quoteText.innerText = result.jokeContent;
     
         quoteBtn.classList.remove("loading");
-        quoteBtn.innerText = "New Quote";
+        quoteBtn.innerText = "New Joke";
     });
 }
 
@@ -24,16 +24,3 @@ quoteBtn.addEventListener("click", randomQuote);
 
 
 
-const options = {
-	method: 'GET',
-	headers: {
-		Authorization: 'chrnMsqPx8Ww',
-		'X-RapidAPI-Key': '3052958391msh06455a139ad1e2bp178c32jsn9a7cbead68fb',
-		'X-RapidAPI-Host': 'random-stuff-api.p.rapidapi.com'
-	}
-};
-
-fetch('https://random-stuff-api.p.rapidapi.com/reddit/RandomMeme?searchType=hot', options)
-	.then(response => response.json())
-	.then(response => console.log(response))
-	.catch(err => console.error(err));
